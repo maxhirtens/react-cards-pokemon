fb20500ed05e175c22a5261e912560c04c01ae6b
Max Hirtenstein <maxhirtens@gmail.com>
2023-04-24 17:32:19 -0700

    pt1 done

---
+++ src/hooks.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const useFlip = (initialState=true) => {
+    const [state, setState] = useState(initialState);
+    const flip = () => {
+        setState(state => !state)
+    }
+    return [state, flip];
+}
+
+export { useFlip };
